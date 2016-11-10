6da3326613d6ac625d345770c05ed3ac51b3e020
TheMesp <webd@mts.net>
2016-11-10 14:27:28 -0600

    Added basic functionality.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnEnter.Location = new System.Drawing.Point(12, 226);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 0;
+            this.btnEnter.Text = "Generate";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.SystemColors.Control;
+            this.lblOutput.Location = new System.Drawing.Point(12, 9);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(260, 111);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = ":^)";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnEnter);
             this.Name = "frmMain";
-            this.Text = "Form1";
+            this.Text = "Math IA";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//David Loewen
+//Random Graphing Application
+//November 10th, 2016
 
 namespace Math_Random_IA
 {
     public partial class frmMain : Form
     {
+        Random random = new Random();
+        float[] rands = new float[4];
+        float generate(float[] r)
+        {
+            //This isn't exactly good style, but I'll only be putting in arrays with 4 indexes (0-3) so it's fine
+            return ((r[0] + r[1]) * (r[2] + r[3]));
+        }
         public frmMain()
         {
             InitializeComponent();
         }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < rands.Length; i++)
+            {
+                rands[i] = (float)random.NextDouble();
+            }
+            lblOutput.Text =
+                " r1 = " + rands[0] +
+                "\r\n r2 = " + rands[1] +
+                "\r\n r3 = " + rands[2] +
+                "\r\n r4 = " + rands[3] +
+                "\r\n rfinal = " + generate(rands);
+
+        }
+
     }
 }
